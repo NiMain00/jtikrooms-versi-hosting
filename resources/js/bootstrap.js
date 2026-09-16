9b5d6cc4884997a742c12b9c586cd570e4fcd083
NiMain00 <nimain@example.com>
2026-09-16 16:38:03 +0800

    Hapus file non-hosting: .github/workflows, tests, vite/package/phpunit, resources css-js, php kosong, database.zip, docs Laravel

--- resources/js/bootstrap.js
+++
@@ -1,7 +0,0 @@
-// resources/js/bootstrap.js
-import axios from 'axios';
-window.axios = axios;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-// Import Bootstrap
-import 'bootstrap';
